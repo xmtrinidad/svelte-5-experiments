31db942bbff9850373718757fa7e5001fdfdcb9e
xmtrinidad <xmtrinidad@gmail.com>
2024-12-27 12:03:11 -0700

    messing with event props

---
+++ src/lib/components/Settings.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    let { 
+        updateColor,
+        updateName 
+    }: {
+        updateColor: (color: string) => void;
+        updateName: (name: string) => void;
+    } = $props();
+
+
+    let color = $state('');
+    let name = $state('');
+</script>
+
+<div class="settings">
+    <div class="input-container">
+        <label for="color">Color Hex</label>
+        <input bind:value={color} type="text" id="color" name="color" placeholder="Color Hex" />
+        <button onclick={() => updateColor(color)}>Update Color</button>
+    </div>
+    <div class="input-container">
+        <label for="name">Color Hex</label>
+        <input bind:value={name} type="text" id="name" name="name" placeholder="Name" />
+        <button onclick={() => updateName(name)}>Update Name</button>
+    </div>
+</div>
---
+++ src/routes/state-thing/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import Settings from "$lib/components/Settings.svelte";
+    const settings = $state({
+        color: "#f0f0f0",
+        name: "THING"
+    });
+
+    function updateColor(newColor: string) {
+        settings.color = newColor;
+    }
+
+    function updateName(newName: string) {
+        settings.name = newName;
+    }
+
+</script>
+
+<div class="things">
+    <Settings {updateColor} {updateName} />
+    <div class="display">
+        <div class="box" style="background-color: {settings.color}"></div>
+        <div class="details">
+            <p>Name: {settings.name}</p>
+        </div>
+    </div>
+</div>
+
+<style>
+    .things {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .display {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 1rem;
+    }
+
+    .box {
+        width: 200px;
+        height: 200px;
+        background-color: #f0f0f0;
+        border-radius: 10px;
+        margin-bottom: 1rem;
+    }
+
+    .details {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
